130b876e7ff7f72dffd4870a3f4a889d6b342111
Assapatt <2426864618@qq.com>
2019-11-28 14:57:00 +0800

    sys-ui 2ed

--- hermitcrab_system/routes/system.js
+++ hermitcrab_system/routes/system.js
@@ -6,4 +6,24 @@ router.get('/system', function(req, res, next) {
   res.render('system', { title: 'System' });
 });
 
+router.get('/system/lanmanage', function(req, res, next) {
+  res.render('lanmanage', { title: 'System' });
+});
+
+router.get('/system/usermanage', function(req, res, next) {
+  res.render('usermanage', { title: 'System' });
+});
+
+router.get('/system/ordermanage', function(req, res, next) {
+  res.render('ordermanage', { title: 'System' });
+});
+
+router.get('/system/officemanage', function(req, res, next) {
+  res.render('officemanage', { title: 'System' });
+});
+
+router.get('/system/srmanage', function(req, res, next) {
+  res.render('srmanage', { title: 'System' });
+});
+
 module.exports = router;
