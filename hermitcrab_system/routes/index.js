e03843971d8a57a6ca72b9d4d02b05cabc1d53be
Assapatt <2426864618@qq.com>
2019-12-02 14:13:54 +0800

    login & data table

--- hermitcrab_system/routes/index.js
+++ hermitcrab_system/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Login' });
 });
+
+router.post('/system', function (req, res, next) {
+  var i = 0;
+  while(i < data.users.length) {
+    if(data.users[i].username === req.body.username && data.users[i].password === req.body.pwd) {
+      console.log("success");
+      res.render('system', {
+        title: 'System'
+      });
+      break;
+    }
+    i++;
+  }
+  if(i==data.users.length){
+    res.end("login error");
+  }
+})
 
 module.exports = router;
