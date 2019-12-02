808a423cf580f51c215b483fe99905d60a71bc0f
Assapatt <2426864618@qq.com>
2019-12-02 14:50:49 +0800

    data table

--- hermitcrab_system/routes/index.js
+++ hermitcrab_system/routes/index.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var data = require('../data.json');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Login' });
 });
 
 router.post('/system', function (req, res, next) {
   var i = 0;
-  while(i < data.users.length) {
-    if(data.users[i].username === req.body.username && data.users[i].password === req.body.pwd) {
+  while (i < data.users.length) {
+    if (data.users[i].username === req.body.username && data.users[i].password === req.body.pwd) {
       console.log("success");
       res.render('system', {
         title: 'System'
@@ -19,9 +19,35 @@ router.post('/system', function (req, res, next) {
     }
     i++;
   }
-  if(i==data.users.length){
+  if (i == data.users.length) {
     res.end("login error");
   }
 })
+
+router.get('/getdata/user', function (req, res, next) {
+  res.json(
+    {
+      "code": 0,
+      "msg": "",
+      "count": 1000,
+      "data": [
+        {
+          "id": 1,
+          "username": "user-00",
+          "sex": "女",
+          "phone": "13365875369",
+          "address": "河北省石家庄市裕华区河北师范大学"
+        },
+        {
+          "id": 2,
+          "username": "user-01",
+          "sex": "男",
+          "phone": "13365875369",
+          "address": "河北省石家庄市裕华区河北师范大学"
+        }
+      ]
+    }
+  )
+});
 
 module.exports = router;
