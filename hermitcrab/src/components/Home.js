002c082478951660bff242be16680ea0fc324699
zhangxintaobb <1309526743@qq.com>
2019-12-03 16:47:33 +0800

    zhangxintao

--- hermitcrab/src/components/Home.js
+++ hermitcrab/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { Link, Switch } from 'react-router-dom';
 import './Home.css'
 import CityList from './CityList'
