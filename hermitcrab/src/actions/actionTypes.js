07fda36860da5bfb6c8fdb0b2cc8be5f37fd7738
zhangxintaobb <1309526743@qq.com>
2019-12-11 09:32:46 +0800

    change person connect SQL

--- hermitcrab/src/actions/actionTypes.js
+++ hermitcrab/src/actions/actionTypes.js
@@ -1,1 +1,2 @@
 export const LOGIN_SUCCESS = 'login_success';
+export const CHANGE_DATA = 'change_data';
--- hermitcrab/src/store/reducer.js
+++ hermitcrab/src/store/reducer.js
@@ -1,14 +1,18 @@
 import {combineReducers} from 'redux';
-import {LOGIN_SUCCESS} from '../actions/actionTypes';
+import {LOGIN_SUCCESS,CHANGE_DATA} from '../actions/actionTypes';
+
 
 let userData={}
-
 function login(state=userData,action){
     switch(action.type){
         case LOGIN_SUCCESS:
             console.log(action.value)
             userData=action.value
-            return action.value
+            return userData
+        case CHANGE_DATA:
+            console.log(action.value)
+            userData=action.value
+            return userData
         default:
             return state
         
