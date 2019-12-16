0da8b606c550785512bbbab1b3b8bf2bed9ebc94
秦继贤 <1653764529@qq.com>
2019-12-16 18:01:28 +0800

    new06

--- hermitcrab/src/App.js
+++ hermitcrab/src/App.js
@@ -17,15 +17,12 @@ import Comment from './container/myself/Comment';
 import Pay from './container/myself/Pay';
 
 import officeinfor from './container/Foundhouse/officeinfor';
-<<<<<<< HEAD
 import Message_jump from './container/myself/Message_jump';
-=======
+
 import srinfor from './container/Foundhouse/srinfor';
-<<<<<<< HEAD
+
 import Info_jump from './container/Foundhouse/Info_jump';
-=======
->>>>>>> 3c81c8c2872c9a5a74a8867790ba119fcc969a1d
->>>>>>> e8313d75894d3e8ba1a71cf671e9e20b871a22bf
+
 
 function App() {
   return (
@@ -35,7 +32,6 @@ function App() {
           
           <Route path='/citylist' component={CityList} />
           <Route path='/myself/pay' component={Pay} />
-          <Route path='/myself/message' component={Message_jump} />
           <Route path='/myself/person' component={Person} />
           <Route path='/info_jump' component={Info_jump} />
           <Route path='/myself/record' component={Record} />
