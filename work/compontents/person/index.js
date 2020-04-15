238b6a3061f5b79dbd25adfa965fadb3d070eee1
zhangxintaobb <1309526743@qq.com>
2020-04-15 13:25:46 +0800

    react-native-router-flux

---
+++ work/compontents/person/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class index extends Component {
+    render() {
+        return (
+            <View>
+                <Text> 个人页 </Text>
+            </View>
+        )
+    }
+}
